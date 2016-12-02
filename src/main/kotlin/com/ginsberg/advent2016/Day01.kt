8d8f930c13ee24c0ba6e0e04bfe08461a38c8546
Todd Ginsberg <todd@ginsberg.com>
2016-12-02 17:00:30 -0600

    Clean up Day
    + Get rid of Movement state machine.
    + Approach is still the same: replay instructions until we run out or cross.
    + Implementation of instructions are manipulation of a State data class now.

--- src/main/kotlin/com/ginsberg/advent2016/Day01.kt
+++ src/main/kotlin/com/ginsberg/advent2016/Day01.kt
@@ -4,7 +4,6 @@
 
 package com.ginsberg.advent2016
 
-import com.ginsberg.advent2016.utils.Movement
 
 /**
  * Advent of Code - Day 1: December 1, 2016
@@ -18,74 +17,73 @@ import com.ginsberg.advent2016.utils.Movement
  */
 class Day01(instructionText: String) {
 
-    private val instructions = stringToInstructions(instructionText)
+    private val instructions = inputToInstructions(instructionText)
+    private data class Instruction(val move: Int = 1, val turn: Char? = null)
+    private data class State(val x: Int = 0,
+                             val y: Int = 0,
+                             val facing: Char = 'N') {
+        val distance by lazy { Math.abs(x) + Math.abs(y) }
+        val coordinates by lazy { Pair(x, y) }
+    }
 
     /**
-     * Problem:
-     * The Document indicates that you should start at the given coordinates
-     * (where you just landed) and face North. Then, follow the provided sequence:
-     * either turn left (L) or right (R) 90 degrees, then walk forward the given
-     * number of blocks, ending at a new intersection.
-     *
-     * There's no time to follow such ridiculous instructions on foot, though,
-     * so you take a moment and work out the destination. Given that you can only
-     * walk on the street grid of the city, how far is the shortest path to the destination?
-     *
-     * [See tests for examples]
-     *
      * How many blocks away is Easter Bunny HQ?
-     *
-     * Solution:
-     * Execute instructions (turn and move) until we run out, calculate the distance.
      */
     fun solvePart1(): Int {
         return instructions
-                .fold(Movement.start()) { carry, next -> carry.turn(next.direction).move(next.amount) }
-                .distance()
+                .fold(State()) { carry, next -> instruct(carry, next) }
+                .distance
     }
 
     /**
-     * Problem:
-     * Then, you notice the instructions continue on the back of the Recruiting Document.
-     * Easter Bunny HQ is actually at the first location you visit twice.
-     *
-     * For example, if your instructions are R8, R4, R4, R8, the first location you visit
-     * twice is 4 blocks away, due East.
-     *
      * How many blocks away is the first location you visit twice?
-     *
-     * Solution:
-     * Turn the list of instructions into step-by-step instructions. Keep executing
-     * steps recursively and collect the locations we've been to. As soon as we detect
-     * we've been somewhere (location is in the set), calculate the distance and return.
      */
     fun solvePart2(): Int {
-        tailrec fun inner(beenTo: Set<Pair<Int,Int>>, path: List<Instruction>, location: Movement): Int {
-            if(beenTo.contains(location.location())) {
-                return location.distance()
-            } else if(path.isEmpty()) {
-                return 0
+        tailrec fun inner(beenTo: Set<Pair<Int,Int>>, path: List<Instruction>, state: State = State()): Int =
+            when {
+                beenTo.contains(state.coordinates) -> state.distance
+                path.isEmpty() -> 0
+                else -> {
+                    inner(beenTo + state.coordinates, path.drop(1), instruct(state, path[0]))
+                }
             }
-            val next = location.turn(path[0].direction).move(path[0].amount)
-            return inner(beenTo + location.location(), path.drop(1), next)
-        }
-        return inner(emptySet(), instructions.flatMap { instructionToSteps(it) }, Movement.start())
+        println(instructionToSteps(instructions))
+        return inner(emptySet(), instructionToSteps(instructions))
     }
 
-    /**
-     * Given a String of instructions separated by commas, turn them
-     * into a list of Instruction objects.
-     *
-     * NOTES: I probably could have done a Pair<Char,Int> instead of a data class,
-     *        but I felt that looked messy. Same thing with consuming
-     *        the input String as a stream, I didn't feel the code to do it was
-     *        clean and easy enough to follow.
-     */
-    private fun stringToInstructions(s: String): List<Instruction> =
-            s.split(", ").map { Instruction(it[0], it.substring(1).toInt()) }
+    private fun instruct(state: State, instruction: Instruction): State {
+        val next = if(instruction.turn == null) state else turn(state, instruction.turn)
+        return move(next, instruction.move)
+    }
 
-    private fun instructionToSteps(instruction: Instruction): List<Instruction> =
-        listOf(instruction.copy(amount = 1)) + (1 until instruction.amount).map { Instruction(amount = 1)}
+    private fun turn(state: State, turn: Char?): State {
+        val next = when (state.facing) {
+            'N' -> if (turn == 'L') 'W' else 'E'
+            'S' -> if (turn == 'L') 'E' else 'W'
+            'E' -> if (turn == 'L') 'N' else 'S'
+            'W' -> if (turn == 'L') 'S' else 'N'
+            else -> state.facing
+        }
+        return state.copy(facing = next)
+    }
 
-    private data class Instruction(val direction: Char = 'X', val amount: Int = 0)
+    private fun move(state: State, amount: Int = 1): State =
+        when(state.facing) {
+            'N' -> state.copy(x = state.x + amount)
+            'S' -> state.copy(x = state.x - amount)
+            'E' -> state.copy(y = state.y + amount)
+            'W' -> state.copy(y = state.y - amount)
+            else -> state
+        }
+
+    private fun inputToInstructions(input: String): List<Instruction> =
+            input.split(", ")
+                 .map{ Instruction(it.substring(1).toInt(), it[0]) }
+
+    private fun instructionToSteps(instruction: List<Instruction>): List<Instruction> =
+            instruction.flatMap {
+                listOf(it.copy(move = 1)) + (1 until it.move).map { Instruction() }
+            }
+
+
 }
