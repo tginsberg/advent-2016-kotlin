c7493d0ed48d6c13c9c4846989dfc08b06d231ac
Todd Ginsberg <todd@ginsberg.com>
2016-12-17 17:01:46 -0600

    Day 13: More sensible algorithms
    + Yeah, A* was fun, and I learned something by implementing it, but it's not practical. This is easier to understand.

--- src/main/kotlin/com/ginsberg/advent2016/Day13.kt
+++ src/main/kotlin/com/ginsberg/advent2016/Day13.kt
@@ -4,6 +4,8 @@
 
 package com.ginsberg.advent2016
 
+import java.util.ArrayDeque
+
 
 /**
  * Advent of Code - Day 13: December 13, 2016
@@ -11,64 +13,60 @@ package com.ginsberg.advent2016
  * From http://adventofcode.com/2016/day/13
  *
  */
-class Day13(favorite: Int, goalX: Int, goalY: Int, val depth: Int = 50) {
+class Day13(favorite: Int, val goalX: Int, val goalY: Int, val depth: Int = 50) {
 
-    private val start = Node(1, 1, favorite)
-    private val goal = Node(goalX, goalY, favorite)
+    private val start = Node(1, 1, 0, favorite)
 
-    fun solvePart1(): Int = aStarGrid()
+    fun solvePart1(): Int = searchGrid()
 
     fun solvePart2(): Int = countStepsToDepth()
 
-    fun aStarGrid(to: Node = goal): Int {
-        var open: Set<Node> = mutableSetOf(start)
-        var closed: Set<Node> = mutableSetOf()
-        var cameFrom: Map<Node, Node> = mutableMapOf()
-        var gScore: Map<Node, Int> = mutableMapOf(start to 0)
-        var fScore: Map<Node, Int> = mutableMapOf(start to start.scoreTo(to))
-        while (open.isNotEmpty()) {
-            val current: Node = open.minBy { fScore[it] ?: Int.MAX_VALUE }!!
-            if (current == to) {
-                return gScore[current]!!
+    fun searchGrid(): Int {
+        var found: Set<Pair<Int, Int>> = emptySet()
+        val queue = ArrayDeque<Node>().apply {
+            add(start)
+        }
+        while (queue.isNotEmpty()) {
+            val current = queue.poll()
+            found += Pair(current.x, current.y)
+            if (current.x == goalX && current.y == goalY) {
+                return current.distance
+            } else {
+                queue.addAll(
+                    current
+                        .neighbors()
+                        .filterNot { found.contains(Pair(it.x, it.y)) }
+                )
             }
-            open -= current
-            closed += current
-            val nextScore = gScore[current]!! + 1
-            current.neighbors()
-                .filterNot { closed.contains(it) }
-                .filterNot { nextScore >= gScore[it] ?: Int.MAX_VALUE }
-                .forEach { n ->
-                    if (!open.contains(n)) open += n
-                    cameFrom = cameFrom.plus(n to current)
-                    gScore = gScore.plus(n to nextScore)
-                    fScore = fScore.plus(n to nextScore + n.scoreTo(goal))
-                }
         }
         return -1
     }
 
     fun countStepsToDepth(): Int {
-        var queue = listOf(start)
-        val seen: MutableSet<Node> = mutableSetOf()
-        while(queue.isNotEmpty()) {
-            val current = queue.first()
-            queue = queue.drop(1)
-            val distance = aStarGrid(current)
-            if(distance <= depth) {
-                seen.add(current)
-                queue += current.neighbors().filter { it !in seen }
-            }
+        var found: Set<Pair<Int, Int>> = emptySet()
+        val queue = ArrayDeque<Node>().apply {
+            add(start)
         }
-        return seen.count()
+        while (queue.isNotEmpty()) {
+            val current = queue.poll()
+            found += Pair(current.x, current.y)
+            queue.addAll(
+                current
+                    .neighbors()
+                    .filter { it.distance <= depth }
+                    .filterNot { found.contains(Pair(it.x, it.y)) }
+            )
+        }
+        return found.size
     }
 
-    data class Node(val x: Int, val y: Int, private val favorite: Int = 10) {
+    data class Node(val x: Int, val y: Int, val distance: Int = 0, private val favorite: Int = 10) {
         fun neighbors(): List<Node> =
             listOf(
-                this.copy(x = x.inc()),
-                this.copy(x = x.dec()),
-                this.copy(y = y.inc()),
-                this.copy(y = y.dec()))
+                this.copy(x = x.inc(), distance = distance.inc()),
+                this.copy(x = x.dec(), distance = distance.inc()),
+                this.copy(y = y.inc(), distance = distance.inc()),
+                this.copy(y = y.dec(), distance = distance.inc()))
                 .filter { it.inBounds() }
                 .filter { it.isOpen() }
 
@@ -77,8 +75,5 @@ class Day13(favorite: Int, goalX: Int, goalY: Int, val depth: Int = 50) {
 
         fun isOpen(): Boolean =
             Integer.bitCount(((x * x) + (3 * x) + (2 * x * y) + y + (y * y)) + favorite) % 2 == 0
-
-        fun scoreTo(node: Node) =
-            Math.abs(x - node.x) + Math.abs(y + node.y)
     }
 }
