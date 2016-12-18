833dfe7c18de9c0bfeec60afd1e43daae341b985
Todd Ginsberg <todd@ginsberg.com>
2016-12-18 08:45:58 -0600

    Day 18: Like a Rogue
    + Formatting. Now stop messing with it. :)

--- src/main/kotlin/com/ginsberg/advent2016/Day18.kt
+++ src/main/kotlin/com/ginsberg/advent2016/Day18.kt
@@ -14,13 +14,13 @@ package com.ginsberg.advent2016
 class Day18(val input: String) {
 
     fun solve(rows: Int): Int =
-        rows(input).take(rows).map { it.count { it == '.' }}.sum()
+        rows(input).take(rows).map { it.count { it == '.' } }.sum()
 
     fun rows(start: String): Sequence<String> =
         generateSequence(start, { nextRow(".$it.") })
 
     private fun nextRow(row: String): String =
         (0..row.length - 3)
-            .map { if (row[it] != row[it+2]) '^' else '.' }
+            .map { if (row[it] != row[it + 2]) '^' else '.' }
             .joinToString("")
 }
