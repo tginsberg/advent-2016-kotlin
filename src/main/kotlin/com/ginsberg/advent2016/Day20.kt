f7703f9f64e6135ddd7e5aad675216473a103542
Todd Ginsberg <todd@ginsberg.com>
2016-12-20 18:34:37 -0600

    Day 20: Firewall Rules
    + Optimization is now a fold and a list manipulation.
    + Part 1 is just find one after the end of the first range, which we can do if the list is optimized.

--- src/main/kotlin/com/ginsberg/advent2016/Day20.kt
+++ src/main/kotlin/com/ginsberg/advent2016/Day20.kt
@@ -12,39 +12,30 @@ package com.ginsberg.advent2016
  *
  */
 class Day20(val input: List<String>) {
-    val ipRanges = optimizeRanges(parseInput())
+    val ipRanges = parseInput()
 
     fun solvePart1(): Long =
-        (0 until ipRanges.size-1 step  2)
-            .filter { !ipRanges[it+1].contains(ipRanges[it].last + 1) }
-            .map { ipRanges[it].last + 1}
-            .first()
+        ipRanges.first().last.inc()
 
     fun solvePart2(upperBound: Long = 4294967295): Long =
-        upperBound.inc() - ipRanges.map{ (it.last - it.first).inc() }.sum()
+        upperBound.inc() - ipRanges.map { (it.last - it.first).inc() }.sum()
 
     private fun parseInput(): List<LongRange> =
-        input
+        optimize(input
             .map { it.split("-") }
             .map { LongRange(it[0].toLong(), it[1].toLong()) }
             .sortedBy { it.first }
+        )
 
-    private fun optimizeRanges(ranges: List<LongRange>): List<LongRange> {
-        tailrec fun inner(done: List<LongRange>, working: LongRange, target: LongRange, rest: List<LongRange>): List<LongRange> =
-            when {
-                rest.isEmpty() && working.combinesWith(target) -> done.plusElement(working.plus(target))
-                rest.isEmpty() -> done.plusElement(working).plusElement(target)
-                working.combinesWith(target) -> inner(done, working.plus(target), rest.first(), rest.drop(1))
-                else -> inner(done.plusElement(working), target, rest.first(), rest.drop(1))
-            }
-
-        return inner(emptyList(), ranges.first(), ranges.drop(1).first(), ranges.drop(2))
-    }
+    private fun optimize(ranges: List<LongRange>): List<LongRange> =
+        ranges.drop(1).fold(ranges.take(1)) { carry, next ->
+            if (carry.last().combinesWith(next)) carry.dropLast(1).plusElement(carry.last().plus(next))
+            else carry.plusElement(next)
+        }
 
     private fun LongRange.plus(other: LongRange): LongRange =
         LongRange(Math.min(this.first, other.first), Math.max(this.last, other.last))
 
     private fun LongRange.combinesWith(other: LongRange): Boolean =
-        other.first in this || this.last+1 in other
-
+        other.first in this || this.last + 1 in other
 }
